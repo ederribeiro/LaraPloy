2732151140980667403b2bd2b4525d07700d63fd
Eder Ribeiro <eder@ederibeiro.com>
2016-03-12 01:56:19 -0300

    Add composer dependencies and ServiceProvider class

---
+++ src/Ederribeiro/Laraploy/LaraployServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+namespace Ederribeiro\Laraploy;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaraployServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+      
+    }
+}
